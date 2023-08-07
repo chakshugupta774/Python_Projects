4b244394ed6b3833617ec57221d7778545c431fe
Varun <varunpandya12a@gmail.com>
2023-08-07 13:00:00 +0530

    Adding a new file

---
+++ collatz.py
@@ -0,0 +1,14 @@
+
+
+def collatx(num):
+    while(num!=1): 
+       if(num%2==0):
+          num=num//2
+          print(num)
+       else:
+          num=3*num+1
+          print(num)
+
+    
+numx= int(input("ENter number: "))
+collatx(numx)
